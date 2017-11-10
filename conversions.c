b989f07593610ae6dff3d00836973b71405bd6f1
Adam Syed <asyed@e1z3r1p14.42.us.org>
2017-11-10 15:46:56 -0800

    Ok

--- conversions.c
+++ conversions.c
@@ -102,7 +102,7 @@ int	charparse(va_list ap, uint8_t caps, t_options *info)
 {
 	if (caps)
 		info->length = 3;
-	ft_putchar(va_arg(ap, unsigned char));
+	ft_putchar((unsigned char)va_arg(ap, wchar_t));
 	return (1);
 }
 
