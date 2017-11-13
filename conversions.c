ce8bb3beb95c4c4ca045ae4c83b124ec03097ffc
Adam Syed <asyed@e1z3r1p14.42.us.org>
2017-11-12 19:02:02 -0800

    Woo fixes

--- conversions.c
+++ conversions.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 22:19:16 by asyed             #+#    #+#             */
-/*   Updated: 2017/11/12 14:47:39 by asyed            ###   ########.fr       */
+/*   Updated: 2017/11/12 18:35:07 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ** Modify all functions to handle 0 as a valid parameter without seg faulting!
 */
 
-// #include <stdio.h>
+#include <stdio.h>
 /*
 ** This could very well be useless
 */
@@ -72,6 +72,11 @@ int	integer(va_list ap, uint8_t caps, t_options *info)
 	num = s_numfetch(ap, info);
 	length = s_n_length(num);
 	i = info->min_width;
+	if (i < info->percision)
+	{
+		i = info->percision;
+		info->padding = 1;
+	}
 	(info->left ? ft_putnbr(num) : 0);
 	while(length < i)
 	{
@@ -85,6 +90,44 @@ int	integer(va_list ap, uint8_t caps, t_options *info)
 	return (1);
 }
 
+
+// int	integer(va_list ap, uint8_t caps, t_options *info)
+// {
+// 	intmax_t	num;
+// 	__int64_t	length;
+// 	int			i;
+// 	int			percision;
+ 
+//  	if (caps)
+//  		info->length = 3;
+// 	num = s_numfetch(ap, info);
+// 	length = s_n_length(num);
+// 	percision = 0;
+// 	if (info->percision > length)
+// 	{
+// 		percision = info->percision - length;
+// 		info->written += percision;
+// 	}
+// 	i = info->min_width - percision;
+// 	while (!info->left && length < i)
+// 	{
+
+// 		ft_putchar((info->padding) ? '0' : ' ');
+// 		length++;
+// 	}
+// 	while (percision-- > 0)
+// 		ft_putchar('0');
+// 	ft_putnbr(num);
+// 	while (info->left && length < i)
+// 	{
+// 		ft_putchar((info->padding) ? '0' : ' ');
+// 		length++;
+// 	}
+// 	info->written += length;
+// 	(void)caps;
+// 	return (1);
+// }
+
 int	charparse(va_list ap, uint8_t caps, t_options *info)
 {
 	if (caps)
@@ -93,6 +136,7 @@ int	charparse(va_list ap, uint8_t caps, t_options *info)
 		ft_unichar(va_arg(ap, wchar_t), info);
 	else
 		ft_putchar((unsigned char)va_arg(ap, wchar_t));
+	info->written++;
 	return (1);
 }
 
@@ -111,15 +155,21 @@ int	hexadec(va_list ap, uint8_t caps, t_options *info)
 	length = ft_strlen(save);
 	i = info->min_width;
 	if (info->left && info->altform && ft_strcmp(save, "0"))
+	{
 		ft_putstr((caps) ? "0X" : "0x");
+		info->written += 2;
+	}
 	(info->left ? ft_putstr(save) : 0);
 	while (length < i)
 	{
 		ft_putchar((info->padding) ? '0' : ' ');
-		length++;	
+		length++;
 	}
 	if (!info->left && info->altform && ft_strcmp(save, "0"))
+	{
 		ft_putstr((caps) ? "0X" : "0x");
+		info->written += 2;
+	}
 	(info->left ? 0 : ft_putstr(save));
 	info->written += length;
 	return (1);
@@ -134,12 +184,16 @@ int	pointeraddr(va_list ap, uint8_t caps, t_options *info)
 	i = 0;
 	hex = va_arg(ap, unsigned long int);
 	save = numbase(hex, 16, caps, &i);
+	if (!*save)
+		*save = '0';
+	i = ft_strlen(save);
 	if (!info->altform)
+	{
 		ft_putstr((caps) ? "0X" : "0x");
-	if (!hex)
-		ft_putchar('0');
-	else
-		ft_putstr(save);
+		i += 2;
+	}
+	ft_putstr(save);
+	info->written += i;
 	return (1);
 }
 
@@ -151,12 +205,19 @@ int	octal(va_list ap, uint8_t caps, t_options *info)
 	int			i;
 
 	i = 0;
+	if (caps)
+		info->length = 3;
 	hex = u_numfetch(ap, info);
 	save = numbase(hex, 8, caps, &i);
 	if (!*save)
 		*save = '0';
 	length = ft_strlen(save);
 	i = info->min_width;
+	if (info->altform && *save != '0')
+	{
+		ft_putchar('0');
+		info->written++;
+	}
 	(info->left ? ft_putstr(save) : 0);
 	while (length < (__uint64_t)i)
 	{
