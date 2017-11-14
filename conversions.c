0346a3e16e62e2ca39e325da17bf8e1d95e87637
Adam Syed <asyed@e1z3r1p12.42.us.org>
2017-11-13 17:06:13 -0800

    Add

--- conversions.c
+++ conversions.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 22:19:16 by asyed             #+#    #+#             */
-/*   Updated: 2017/11/13 16:42:13 by asyed            ###   ########.fr       */
+/*   Updated: 2017/11/13 17:06:08 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -209,8 +209,8 @@ int	octal(va_list ap, uint8_t caps, t_options *info)
 		info->length = 3;
 	hex = u_numfetch(ap, info);
 	save = numbase(hex, 8, caps, &i);
-	// if (!save)
-	// 	save = "0";
+	if (!save)
+		save = "0";
 	length = ft_strlen(save);
 	i = info->min_width;
 	if (info->altform && *save != '0')
